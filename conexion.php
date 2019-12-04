9f6cf43e08bb7d2c5488701f79feed97ae0f4272
Conhi <ofmu.cohe000601@gmail.com>
2019-12-04 15:38:07 -0600

    gg

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexionDB = new mysqli("localhost", "root", "TateTipycsnow", "graduacion4b");
+$conexionDB = new mysqli("localhost", "root", "Wigetta777", "graduacion4b");
 /*
 $conexionDB = new mysqli("sql313.byethost.com", "b16_24823612", "AssasinsCreed18", "b16_24823612_Graduacion");
 */
